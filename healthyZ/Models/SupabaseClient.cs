234ba27e3f4a9644b47911c482de57488aac573e
若瑋 李 <4b190142@office.stust.edu.tw>
2025-07-02 11:04:47 +0800

    新增註冊頁面功能及 Supabase 整合
    在 `RegisterPage.xaml` 中更新 UI，新增使用者名稱、帳號、密碼、生日、身高和體重的輸入欄位，並更新按鈕文字為「註冊」。在 `RegisterPage.xaml.cs` 中實作註冊邏輯，檢查輸入有效性及帳號重複性，並計算 BMI。新增 `Supabase` 套件引用，並在 `login.cs` 中定義用戶資料模型。最後，新增 `SupabaseClient` 類別以初始化 Supabase 客戶端。

---
+++ healthyZ/Models/SupabaseClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace healthyZ.Models
+{
+    public class SupabaseClient
+    {
+        // 連線登入 Supabase 共用函式
+        public Supabase.Client GetClient()
+        {
+            // 初始化 Supabase 客戶端
+            Supabase.Client _client;
+            _client = new Supabase.Client(
+                "https://zgajpjoewcbijoplqnti.supabase.co",
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnYWpwam9ld2NiaWpvcGxxbnRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0MjAwMTMsImV4cCI6MjA2Njk5NjAxM30.RIuoFbPdEKAYrn4lw7Ei-VkoKx44dnnk9pdP-G8GX3g",
+                new Supabase.SupabaseOptions { AutoConnectRealtime = true });
+            return _client;
+        }
+    }
+}
