44b585f5d65c7fe94b00e0f923ff4e075b0f8ed5
user <user@LAPTOP-UKES3E79>
2025-07-03 17:25:37 +0800

    新增 MapAI 方法並優化登入頁面格式
    在 `AnalyzeImageContent.cs` 中新增了一個名為 `MapAI` 的非同步方法，該方法負責呼叫 Gemini API 以生成內容。此方法構建了一個請求主體，將用戶的訊息作為參數，並將其序列化為 JSON 格式，然後使用 `HttpClient` 發送 POST 請求，最後返回 API 回應的內容。
    
    在 `LoginPage.xaml` 中，對於帳號和密碼的輸入框進行了格式化調整，提升了代碼的可讀性。

--- healthyZ/AI/AnalyzeImageContent.cs
+++ healthyZ/AI/AnalyzeImageContent.cs
@@ -70,5 +70,48 @@ namespace healthy.AI
             }
         }
 
+        public async Task<string> MapAI(string userMessage)
+        {
+            const string ApiUrl2 = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
+            var requestBody = new
+            {
+                contents = new[]
+                    {
+                    new
+                    {
+                        parts = new object[]
+                        {
+                            new{ text = userMessage },
+                            //1. 分析照片的提示詞, 必須仔細修正, Gemini才能回傳符合需要的結果
+                            //new { text = "我現在位置是({latitude},{longitude})，提供距離2公里內評分最高五家健康餐廳\r\n只以下格式回傳資料, 我要在程式中讀取\r\n{ Name = \"茶米風健康餐盒東橋店（身心靈都健康）\", Address = \"台南市永康區東橋一路272號\", Latitude = {latitude}, Longitude = {longitude} }" },
+                        }
+
+                    }
+                }
+            };
+
+            //送至Gemini API的呼叫內容要轉換成Json格式, 再包裝為送出的Http需求內容
+            var json = JsonSerializer.Serialize(requestBody);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //將包裝好的Http需求內容透過HttpClient的PostAsync方法送到Gemini API執行, 並取得回應
+            var response = await _client.PostAsync($"{ApiUrl2}?key={ApiKey}", content);
+            response.EnsureSuccessStatusCode();
+
+            //取得回應內容, 並轉換成Json格式
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var responseJson = JsonSerializer.Deserialize<JsonElement>(responseBody);
+
+            //回傳分析結果
+            return responseJson.GetProperty("candidates")[0]
+                            .GetProperty("content")
+                            .GetProperty("parts")[0]
+                            .GetProperty("text")
+                            .GetString();
+        }
+
     }
+
+
 }
